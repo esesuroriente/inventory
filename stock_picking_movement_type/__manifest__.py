4bdb075876f88b973bbc37aadec9faca91f60e5e
esesurorientecauca.sistemas <esesurorientecauca.sistemas@gmail.com>
2019-11-26 09:31:15 -0500

    [ADD] Add module, movement type

---
+++ stock_picking_movement_type/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 - ESE SURORIENTE CAUCA < >
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock picking movement type',
+    'version': '13.0.1.0.0-beta.1',
+    'author': 'ESE SURORIENTE CAUCA',
+    'license': 'AGPL-3',
+    'category': 'Stock',
+    'depends': [
+        'stock'
+    ],
+    'data': [
+        'views/stock_picking_view.xml',
+
+    ],
+    'installable': True,
+    'auto_install': False,
+}
