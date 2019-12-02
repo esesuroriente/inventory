edd130812eabb20d19fac06e8c9dc813686803a2
esesurorientecauca.sistemas <esesurorientecauca.sistemas@gmail.com>
2019-12-02 14:53:00 -0500

    [FIX] Change field type and add status

--- product_medicament_lots/models/stock_production_lot.py
+++ product_medicament_lots/models/stock_production_lot.py
@@ -6,8 +6,16 @@ class ProductTemplate(models.Model):
     _inherit = 'stock.production.lot'
 
     laboratory = fields.Char(tracking=True)
-    register_invima = fields.Text(tracking=True)
+    register_invima = fields.Char(tracking=True)
     cums = fields.Char(tracking=True)
-    consecutive_cums = fields.Char(tracking=True)
+    cums_consecutive = fields.Char(tracking=True)
+    cums_status = fields.Selection([('vigente', 'Vigente'),
+                                    ('vencido', 'Vencido'),
+                                    ('tramite', 'Trámite de renovación'),
+                                    ('pfuerza', 'Pérdida de fuerza'),
+                                    ('otro', 'Otros estados')],
+                                      string='cum status',
+                                      tracking=True
+                                )
 
     manufacturing_date = fields.Datetime(tracking=True)
