b965414f05d2e2b1eaf3c50965fc3dcb5d29b0c9
esesurorientecauca.sistemas <esesurorientecauca.sistemas@gmail.com>
2019-12-03 10:40:16 -0500

    [DEL] stock picking vender invoice fields

--- stock_picking_vendor_invoice_fields/models/stock_picking.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models, api
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'stock.picking'
-
-    vendor_invoice_date = fields.Date()
-    vendor_invoice_number = fields.Char()
