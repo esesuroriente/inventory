25afb1fabfcd5aa38861f93198ac2510844e9914
esesurorientecauca.sistemas <esesurorientecauca.sistemas@gmail.com>
2019-12-10 10:11:22 -0500

    [FIX] modify fields report

--- stock_report_input_medicament/__manifest__.py
+++ stock_report_input_medicament/__manifest__.py
@@ -7,16 +7,18 @@
     'version': '13.0.1.0.0-beta.2',
     'author': 'ESE SURORIENTE CAUCA',
     'license': 'AGPL-3',
-    'category': 'Stock',
+    'category': 'Other',
     'depends': [
         'stock',
 
         'product_medicament',
         'product_medicament_lots',
-        'stock_picking_vendor_invoice_fields'
+        'stock_picking_vendor_remission_fields'
     ],
     'data': [
         'report/report_stock_input_medicament.xml',
+
+        'data/stock_report_input_medicament_data.xml',
 
         'security/ir.model.access.csv',
     ],
